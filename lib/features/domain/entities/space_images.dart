cd57ff0c355b3028c4b35dd9790249620d887ffa
Emilson Silva <emilsongyn17@gmail.com>
2022-11-09 18:59:16 -0300

    Ajustes

--- lib/features/domain/entities/space_images.dart
+++ lib/features/domain/entities/space_images.dart
@@ -1,6 +1,23 @@
 import 'package:equatable/equatable.dart';
 
 class SpaceImages extends Equatable {
+  final String description;
+  final String mediaType;
+  final String title;
+  final String mediaUrl;
+
+  const SpaceImages({
+    required this.description,
+    required this.mediaType,
+    required this.title,
+    required this.mediaUrl,
+  });
+
   @override
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [
+        description,
+        mediaType,
+        title,
+        mediaUrl,
+      ];
 }
