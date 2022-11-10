201175026dcecb3d800aadb6f351bf7aaae32fd7
Emilson Silva <emilsongyn17@gmail.com>
2022-11-10 09:26:16 -0300

    Ajustes

--- test/features/domain/usecases/get_space_media_usecase_test.dart
+++ test/features/domain/usecases/get_space_media_usecase_test.dart
@@ -1,3 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_clean_architeture/features/domain/repositories/space_media_repository.dart';
+import 'package:tdd_clean_architeture/features/domain/usecases/get_space_media_usecase.dart';
+
+class MockSpaceMediaRepository extends Mock implements ISpaceMediaRepository {}
+
 void main() {
-  GetspaceMediaUseCase usecase;
+  GetspaceMediaUsecase usecase;
+  ISpaceMediaRepository repository;
+
+  setUp(() {
+    repository = MockSpaceMediaRepository();
+    usecase = GetspaceMediaUsecase(repository);
+  });
+
+  test('should get space media for a given date from the repository', () {
+    when(() => MockSpaceMediaRepository());
+  });
 }
